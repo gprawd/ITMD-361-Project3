30c018a0e59d34d3c1be2b45d59892473b389758
Greg Prawdzik <gprawd@hotmail.com>
2019-11-11 17:28:34 -0600

    added locations and directions to map

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -5,9 +5,9 @@ function initMap() {
     var uluru4 = {lat: 41.707, lng: -87.904};
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 10, center: uluru4});
-    var marker = new google.maps.Marker({position: uluru, map: map});
-    var marker2 = new google.maps.Marker({position: uluru2, map: map});
-    var marker3 = new google.maps.Marker({position: uluru3, map: map});
+    var marker = new google.maps.Marker({position: uluru, map: map, icon: 'assets/media/1.png'});
+    var marker2 = new google.maps.Marker({position: uluru2, map: map, icon: 'assets/media/2.png'});
+    var marker3 = new google.maps.Marker({position: uluru3, map: map, icon: 'assets/media/3.png'});
     var transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(map);    
   }
