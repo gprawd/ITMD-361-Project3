f1582f7ade3c3535121e0e4b2a36dd384fbd3a3b
Greg Prawdzik <gprawd@hotmail.com>
2019-11-11 17:04:02 -0600

    added locations

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -1,7 +1,14 @@
 function initMap() {
     var uluru = {lat: 41.538, lng: -88.171};
+    var uluru2 = {lat: 41.885, lng: -87.631};
+    var uluru3 = {lat: 41.831, lng: -87.626};
+    var uluru4 = {lat: 41.707, lng: -87.904};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 10, center: uluru});
+        document.getElementById('map'), {zoom: 10, center: uluru4});
     var marker = new google.maps.Marker({position: uluru, map: map});
+    var marker2 = new google.maps.Marker({position: uluru2, map: map});
+    var marker3 = new google.maps.Marker({position: uluru3, map: map});
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);    
   }
   
